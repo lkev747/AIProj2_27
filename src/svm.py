a820b402e6185b3f99bcb19a04c6b9a2ae49be93
lkev747 <lkev747@gmail.com>
2017-10-23 15:44:22 -0400

    Progress Towards SVM

--- src/svm.py
+++ src/svm.py
@@ -5,6 +5,9 @@
 import util
 PRINT = True
 
+from sklearn import svm
+
+
 class SVMClassifier:
   """
   svm classifier
@@ -12,18 +15,40 @@ class SVMClassifier:
   def __init__( self, legalLabels):
     self.legalLabels = legalLabels
     self.type = "svm"
+    self.clf = svm.LinearSVC()
       
   def train( self, trainingData, trainingLabels, validationData, validationLabels ):
-    print "Starting iteration ", iteration, "..."
-    for i in range(len(trainingData)):
+    
+    ## Added Iterations Loop
+    for iteration in range(self.max_iterations):
+      print "Starting iteration ", iteration, "..."
+      
+      ## --- Kevin, Ely : Train SVM on trainingData --- ##
+      self.clf.fit(trainingData, trainingLabels)
+      ## --- End Kevin, Ely --- ##
+      
+      ## --- Kevin, Ely : Do Validation Test --- ##
+      val_guesses = SVMClassifier.classify(self, validationData)
+      
+      correct = 0
+      for i in range(0, len(val_guesses)):
+        if val_guesses[i] == validationLabels[i]:
+          correct += 1
+      
+      print "Number Correct:", correct
+      ## --- End Kevin, Ely --- ##
+      
+      for i in range(len(trainingData)): 
         "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
     
   def classify(self, data ):
     guesses = []
     for datum in data:
-      # fill predictions in the guesses list
       "*** YOUR CODE HERE ***"
+      ## Kevin, Ely ##
+      guesses.append(self.clf.predict(datum))
+      ## End Kevin, Ely ##
       util.raiseNotDefined()
       
     return guesses
